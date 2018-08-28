e0145b24a40f6d2bdb32a6f066aba3df6030878a
Tyler Lutz <tyler@tylerlutz.com>
2018-08-28 15:22:53 -0400

    Updated Client to Create Drawing

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,16 @@
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:8000');
 
-function subscribeToTimer(cb){
-    socket.on('timer',timestamp => cb(timestamp));
-    socket.emit('subscribeToTimer',1000);
+function subscribeToDrawings(cb){
+    socket.on('drawing',cb);
+    socket.emit('subscribeToDrawings',1000);
+}
+
+function createDrawing(name){
+    socket.emit('createDrawing',{name});
 }
 
 export{
-    subscribeToTimer
+    subscribeToDrawings,
+    createDrawing
 };
