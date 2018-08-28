0e242a05b2a517f2eb213b413f12dd3890a6f898
Tyler Lutz <tyler@tylerlutz.com>
2018-08-28 14:46:12 -0400

    Simple Timer update using Socket.IO

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import {subscribeToTimer} from './api';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    subscribeToTimer((timestamp) => {
+      this.setState({
+        timestamp
+      });
+    });
+  }
+  state = {
+    timestamp: 'no timestamp set'
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Our awesome drawing app</h2>
+        </div>
+        This is the value of the timer timestamp: {this.state.timestamp}
+      </div>
+    );
+  }
+}
+
+export default App;
